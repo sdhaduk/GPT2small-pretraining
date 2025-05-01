0abf97674e89443ee24c8eb6df42e1fec2a4f878
Sagar <sagardhaduk1@gmail.com>
2025-04-30 19:49:34 -0500

    added generate function with temperature and topk

--- utils.py
+++ utils.py
@@ -37,29 +37,26 @@ def token_ids_to_text(token_ids, tokenizer):
     return tokenizer.decode(flat.tolist())
 
 
-def generate_text_simple(model, idx, max_new_tokens, context_size):
-
-    for _ in range(max_new_tokens):
-        idx_cond = idx[:, -context_size:]
-        with torch.no_grad():
-            logits = model(idx_cond)
-        logits = logits[:, -1, :]
-        idx_next = torch.argmax(logits, dim=-1, keepdim=True)
-        idx = torch.cat((idx, idx_next), dim=1)
-
-    return idx
-
-
-def generate_and_return_sample(model, tokenizer, device, start_context):
+def generate_and_return_sample(model, tokenizer, device, start_context,
+                                max_new_tokens=12, temperature=0.7, top_k=30, eos_id=50256):
     model.eval()
-    context_size = model.pos_emb.weight.shape[0]
+
+    context_size = model.pos_emb.weight.shape[0]  # context window length
     encoded = text_to_token_ids(start_context, tokenizer).to(device)
+
     with torch.no_grad():
-        token_ids = generate_text_simple(
-            model=model, idx=encoded,
-            max_new_tokens=20, context_size=context_size
+        token_ids = generate(
+            model=model,
+            idx=encoded,
+            max_new_tokens=max_new_tokens,
+            context_size=context_size,
+            temperature=temperature,
+            top_k=top_k,
+            eos_id=eos_id
         )
-    decoded_text = token_ids_to_text(token_ids, tokenizer).replace("\n", " ")
+
+    # Convert token IDs back to text
+    decoded_text = token_ids_to_text(token_ids[0], tokenizer).replace("\n", " ")
     model.train()
     return decoded_text
     
@@ -99,3 +96,41 @@ def evaluate_model(model, train_loader, val_loader, device, eval_iter):
             val_loader, model, device, num_batches=eval_iter)
     model.train()
     return train_loss, val_loss
+
+def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+
+    # For-loop is the same as before: Get logits, and only focus on last time step
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        # New: Filter logits with top_k sampling
+        if top_k is not None:
+            # Keep only top_k values
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+
+        # New: Apply temperature scaling
+        if temperature > 0.0:
+            logits = logits / temperature
+
+            # Apply softmax to get probabilities
+            probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+
+        # Otherwise same as before: get idx of the vocab entry with the highest logits value
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+        if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+            break
+
+        # Same as before: append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)
+
+    return idx
